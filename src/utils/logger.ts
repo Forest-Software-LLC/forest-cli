acf33dca4d49d3a436f706f4f709f2eb2d2f4570
Harrison Lewis <therealstratiz@gmail.com>
2025-06-12 22:42:49 -0700

    feat: Message.emit

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -15,6 +15,10 @@ export function info(msg: string) {
     console.log(`${chalk.cyan('›')} ${chalk.cyan(msg)}`);
 }
 
+export function warn(msg: string) {
+    console.warn(`\n${chalk.yellow('⚠')} ${chalk.yellow.bold(msg)}`);
+}   
+
 export class Message {
     private spinner: Ora;
 
@@ -31,6 +35,22 @@ export class Message {
         this.spinner.stop();
     }
 
+    emit(type: 'success' | 'fail' | 'info' | 'warn' = 'info', message: string) {
+        switch (type) {
+            case 'success':
+                this.success(message);
+            case 'fail':
+                this.fail(message);
+            case 'info':
+                this.info(message);
+            case 'warn':
+                this.warn(message);
+        }
+
+        this.spinner = ora(this.message).start();
+        this.spinner.color = 'green';
+    }
+
     success(message?: string) {
         this.spinner.stopAndPersist({
             symbol: `${chalk.green('🌳')}`,
@@ -44,6 +64,13 @@ export class Message {
             text: `${chalk.red.bold(message)}`,
         });
     }
+
+    warn(message?: string) {
+        this.spinner.stopAndPersist({
+            symbol: `${chalk.yellow('⚠️ ')}`,
+            text: `${chalk.yellow.bold(message)}`,
+        });
+    }                
 
     info(message?: string) {
         this.spinner.stopAndPersist({
